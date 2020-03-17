0ed6aed5ebba52ac2e0587b8e9983d58009ea2fd
Vignesh <59384973+Vignesh-ab2@users.noreply.github.com>
2020-03-17 15:32:04 +0530

    increment operator and decrement operators

---
+++ task/src/taskjava/OperatorExample.java
@@ -0,0 +1,13 @@
+package taskjava;
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+		int a=10;  
+		System.out.println(a++);
+		System.out.println(a--);
+		System.out.println(++a + ++a);
+		System.out.println(a);
+	}
+
+}
